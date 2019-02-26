c94841d5159694d044dedab09e0e4e32ce5f7863
Irina Nikiforova <gleensande@gmail.com>
2019-02-26 23:54:55 +0300

    new checkbox generated in js

--- components/checkbox.js
+++ components/checkbox.js
@@ -7,25 +7,43 @@ export class Checkbox {
 		id = '0',
 		size = 'small',
 		handler = noop,
+		label = "",
 	} = {}) {
 		this._parent = parent;
 		this._id = id;
-		this._size = 'small';
+		this._size = size;
 		this._handler = handler;
+		this._label = label;
 	}
+  
+	<div class="checkbox small">
+        <input type="checkbox" id="checkboxInput1"/>
+        <label for="checkboxInput1"></label>
+    </div>
 
 	render() {
+		const outerDiv = document.createElement('div');
+		div.classList.add('checkbox');
+		
+		if (this._size === 'small') {
+			div.classList.add('small');
+		} else if (this._size === 'big') {
+			div.classList.add('big');
+		} else {
+			div.classList.add('small');
+		}
+		
 		const checkbox = document.createElement('input');
 		checkbox.type = 'checkbox';
-		checkbox.classList.add('checkbox');
-		if (this._size === 'small') {
-			checkbox.classList.add('small');
-		} else if (this._size === 'big') {
-			checkbox.classList.add('big');
-		}
+		checkbox.id = this._id;
 
-		checkbox.addEventListener(click, handler);
+		const label = document.createElement('label');
+		label.for = this._id;
 	
-		this._parent.appendChild(checkbox);	
+		outerDiv.addEventListener(click, handler);
+	
+		this._parent.appendChild(outerDiv);
+		outerDiv.appendChild(checkbox);
+		outerDiv.appendChild(label);
 	}
 }
