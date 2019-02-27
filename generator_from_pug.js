076e39567a179cc4fe97fd9495843a81731cc88b
sergeychur <churicovser@mail.ru>
2019-02-27 19:50:29 +0300

    image and upload js done, css for image, js generator by .pug written

--- generator_from_pug.js
+++ generator_from_pug.js
@@ -17,5 +17,5 @@ const pug = require('pug');
 // Compile the template to a function string
 const jsFunctionString = pug.compileFileClient(process.argv[2], {name: path.parse(process.argv[2]).name});
 
-// Maybe you want to compile all of your templates to a templates.js file and serve it to the client
+// write to out file
 fs.writeFileSync(process.argv[3], jsFunctionString);
