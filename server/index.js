5652dfe2bd23760b38b51ee5459bc1f1725ba02e
Tsaanstu <atasunts.vladimir@yandex.ru>
2019-03-11 01:57:09 +0300

    init

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 	return res;
 });
 
-let port = 3000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, function () {
 	console.log(`Server listening port ${port}`);
