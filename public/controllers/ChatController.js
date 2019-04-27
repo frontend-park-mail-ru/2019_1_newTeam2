89816d4812c5eaf85a88a5b4405efda7e161305f
Irina Nikiforova <gleensande@gmail.com>
2019-04-27 16:44:58 +0300

    unlogined dont have permission

--- public/controllers/ChatController.js
+++ public/controllers/ChatController.js
@@ -1,6 +1,8 @@
 import {Controller} from '/controllers/Controller.js';
 import {Chat} from '/views/Chat/Chat.js';
+import {UserModel} from '/models/UserModel.js';
 import auth from '/models/AuthModel.js';
+import bus from '/services/bus.js';
 
 export class ChatController extends Controller {
     index() {
@@ -10,9 +12,12 @@ export class ChatController extends Controller {
         this.listeners = new Set ([
             ['logged-in', this._onloggedin],
             ['logged-out', this._onloggedout],
+            ['ws-message-received', this._onmessagereceived],
         ]);
 
         super.subscribeAll();
+
+        this.model = new UserModel();
     }
 
     _onloggedin() {
@@ -21,5 +26,35 @@ export class ChatController extends Controller {
     
     _onloggedout() {
         this.view.render({authorised: false});
+    }
+
+    _notify(data) {
+        if (!('Notification' in window)) {
+            console.error('notifications not supported');
+            return;
+        }
+
+        if (Notification.permission === 'granted') {
+            new Notification(data);
+            return;
+        }
+
+        if (Notification.permission !== 'denied') {
+            Notification
+                .requestPermission()
+                .then((permission) => {
+                    if (permission === 'granted') {
+                        new Notification(data);
+                        return;
+                    }
+                });
+        }
+    }
+
+    _onmessagereceived(data) {
+        if(document.hidden) {
+            this._notify(data.message);
+        }
+        bus.emit('name-got');
     }
 }
